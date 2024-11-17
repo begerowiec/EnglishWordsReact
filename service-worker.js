09e71492577c34f0c0853e83ad420cf9626f927a
begerowiec <agadrianekk16@gmail.com>
2024-11-18 00:48:40 +0100

    Updates

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+// Zdarzenie 'install'
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Zainstalowany');
+  });
+  
+  // Zdarzenie 'activate'
+  self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Aktywny');
+  });
+  
+  // Zdarzenie 'fetch'
+  self.addEventListener('fetch', (event) => {
+    // Możesz dodać tutaj obsługę cache'owania żądań.
+  });
+  
